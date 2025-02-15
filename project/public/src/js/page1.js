bba4a1966884aad6841db2a0ef7d0388fbc999c2
nayeon-3 <sghnyyj@gmail.com>
2025-02-15 11:34:38 +0900

    clean

---
+++ project/public/src/js/page1.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const priceElement = document.querySelector(".price");
+
+// 가격 정보를 클릭하면 알림창이 뜨는 기능 추가
+if (priceElement) {
+    priceElement.addEventListener("click", function () {
+        alert("월세 범위는 26~40만원입니다.");
+    });
+}
+});
